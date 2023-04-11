81102cb04176fafa084b95bd1e3478cdcc0b4d33
Iguta <davidiguta@gmail.com>
2023-04-11 23:34:45 +0300

    Ecommerce using memory instead of database

---
+++ Lab9_Nyaundi/src/main/java/edu/cscc/Lab9_Nyaundi/Lab9NyaundiApplication.java
@@ -0,0 +1,13 @@
+package edu.cscc.Lab9_Nyaundi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9NyaundiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab9NyaundiApplication.class, args);
+	}
+
+}
